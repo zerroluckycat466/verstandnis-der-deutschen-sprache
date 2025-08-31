7778554627a267672a663a126ddc3599194bf729
zerroluckycat466 <m98jtlgb0nbs@hotmail.com>
2025-08-31 23:05:54 +0800

    Refactor to use path module for safer file path construction.

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(PORT, () => {
